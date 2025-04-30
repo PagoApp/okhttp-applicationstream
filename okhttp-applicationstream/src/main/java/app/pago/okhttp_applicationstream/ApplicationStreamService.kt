411e93ff5ffe1ec36620632701b978d37321af39
Adrian Pascu <me.adi.hf@gmail.com>
2025-04-30 15:38:08 +0300

    Expose okhttp client

--- okhttp-applicationstream/src/main/java/app/pago/okhttp_applicationstream/ApplicationStreamService.kt
+++ okhttp-applicationstream/src/main/java/app/pago/okhttp_applicationstream/ApplicationStreamService.kt
@@ -16,19 +16,8 @@ import java.io.IOException
 
 class ApplicationStreamService(
     private val baseUrl: String,
-    interceptors: List<Interceptor> = emptyList()
+    private val client: OkHttpClient = OkHttpClient(),
 ) {
-    private val client: OkHttpClient
-
-    init {
-        val builder = OkHttpClient.Builder()
-        interceptors.forEach { interceptor ->
-            builder.addInterceptor(interceptor)
-        }
-
-        client = builder.build()
-    }
-
     // Parse the data in the buffer and return the list of extracted objects and what is left in the buffer
     // if there was an incomplete object in there
     private fun <T> parseDataInBuffer(
